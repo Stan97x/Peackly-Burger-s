3cc88cce9c35996ee665d2ad56bd2afc0b9c4baa
Stan <105353221+Stan97x@users.noreply.github.com>
2024-11-27 14:34:15 +0100

    Add files via upload

---
+++ formule.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Peackly Burger's</title>
+	<link rel="stylesheet" href="assets/css/formule.css">
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="sidebar">
+    <img class="logo"src='assets/images/PeacklyBurger.png'></a>
+        <ul>
+            <li><a href="#"><i class="fas fa-home"></i>Home</a></li>
+            <li><a href="#"><i class="fas fa-user"></i>Profile</a></li>
+            <li><a href="#"><i class="fas fa-address-card"></i>About</a></li>
+            <li><a href="#"><i class="fas fa-project-diagram"></i>portfolio</a></li>
+            <li><a href="#"><i class="fas fa-blog"></i>Blogs</a></li>
+            <li><a href="#"><i class="fas fa-address-book"></i>Contact</a></li>
+            <li><a href="#"><i class="fas fa-map-pin"></i>Map</a></li>
+        </ul> 
+    </div>
+    <div class="main_content">
+
+        <div class="info">
+          </div>
+      </div>
+    </div>
+</div>
+
+</body>
+</html>
